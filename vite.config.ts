a532a5a613759e419243f347f2af50d1846b0b90
Benjoe Oates Carandang <benjoe.carandang@gmail.com>
2025-07-25 03:25:45 +0800

    added domain name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/grow-garden-weight-calculator/',
+  base: '/',
   plugins: [react(), tailwindcss()]
 });
